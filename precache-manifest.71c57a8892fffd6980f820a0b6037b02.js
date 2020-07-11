5a49965f26a639fe2fd7858c46c47a740091eed1
donttestnodi <53544100+jmxnodebe@users.noreply.github.com>
2020-07-11 23:00:32 +0100

    Updates

---
+++ precache-manifest.71c57a8892fffd6980f820a0b6037b02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9cad026f1b83e3bec5e40cc319894a",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "f82a67d34fdec1606407",
+    "url": "/todo-app/static/css/main.7f086564.chunk.css"
+  },
+  {
+    "revision": "1d85bdfac6195695a0f7",
+    "url": "/todo-app/static/js/2.7a9f1bb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.7a9f1bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82a67d34fdec1606407",
+    "url": "/todo-app/static/js/main.113a9d7e.chunk.js"
+  },
+  {
+    "revision": "2e09dffda2050be9d096",
+    "url": "/todo-app/static/js/runtime-main.3deb19b9.js"
+  }
+]);
